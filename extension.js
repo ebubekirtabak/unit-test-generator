15c85c0c23b7d1d3c774820376c01cb6464ce30b
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-03 19:30:26 +0300

    new subsubscribtion added to the extension.generateUnitTest command

--- extension.js
+++ extension.js
@@ -2,12 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -18,13 +16,14 @@ function activate(context) {
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
+	let disposableUnitTestGenerate = vscode.commands.registerCommand('extension.generateUnitTest', function () {
+		vscode.window.showInformationMessage('Generate UnitTest ');
+	});
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableUnitTestGenerate);
 }
 exports.activate = activate;
 
