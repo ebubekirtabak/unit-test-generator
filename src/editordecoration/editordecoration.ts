54792fa7eda8cbe1695f7715857c7ec967b538e8
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-20 09:26:41 +0300

    editordecoration added.

---
+++ src/editordecoration/editordecoration.ts
@@ -0,0 +1,13 @@
+import { Disposable, ExtensionContext } from "vscode";
+
+export class EditorDecoration implements Disposable {
+
+  dispose() {
+    throw new Error("Method not implemented.");
+  }
+
+  constructor(context: ExtensionContext) {
+
+  }
+
+}
