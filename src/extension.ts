8ca3f60423fef41ab9ac7b87492c4578209981aa
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-20 17:28:13 +0300

    typescript parser added.

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { TypeScriptParser } from './parsers/TypeScriptParser';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -6,6 +7,7 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "tdd-generator" is now active!');
 
 	let disposable = vscode.commands.registerCommand('extension.generateUnitTest', () => {
+    new TypeScriptParser().parse();
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
---
+++ src/parsers/TypeScriptParser.ts
@@ -0,0 +1,56 @@
+import { Constants } from "../constants";
+
+export class TypeScriptParser {
+
+  testCode = `
+  import { workspace, Uri } from "vscode";
+import {
+    getInitialVSCodeMetricsConfiguration,
+    IVSCodeMetricsConfiguration
+} from "../metrics/common/VSCodeMetricsConfiguration";
+
+export class AppConfiguration {
+    constructor() {}
+    get extensionName() {
+        return "codemetrics";
+    }
+    public toggleCodeMetricsForArrowFunctionsExecuted: boolean = false;
+    public codeMetricsForArrowFunctionsToggled: boolean = true;
+    public codeMetricsDisplayed: boolean = true;
+
+    getCodeMetricsSettings(resource: Uri): IVSCodeMetricsConfiguration {
+        var settings = workspace.getConfiguration(this.extensionName, resource);
+        const resultingSettings = getInitialVSCodeMetricsConfiguration();
+        for (var propertyName in resultingSettings) {
+            var property = "nodeconfiguration." + propertyName;
+            if (settings.has(property)) {
+                resultingSettings[propertyName] = settings.get(property);
+                continue;
+            }
+            property = "basics." + propertyName;
+            if (settings.has(property)) {
+                resultingSettings[propertyName] = settings.get(property);
+                continue;
+            }
+        }
+        for (var propertyName in resultingSettings.LuaStatementMetricsConfiguration) {
+            property = "luaconfiguration." + propertyName;
+            if (settings.has(property)) {
+                resultingSettings.LuaStatementMetricsConfiguration[propertyName] = settings.get(property);
+                continue;
+            }
+        }
+        if (this.toggleCodeMetricsForArrowFunctionsExecuted) {
+            resultingSettings.MetricsForArrowFunctionsToggled = this.codeMetricsForArrowFunctionsToggled;
+        }
+        return resultingSettings;
+    }
+}
+  `;
+
+  parse() {
+    const regexp = new RegExp(Constants.FUNCTION_NAME_REGEX);
+    const functions = regexp.test(this.testCode);
+    console.log(functions);
+  }
+}
