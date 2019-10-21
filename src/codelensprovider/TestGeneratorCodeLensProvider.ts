051ab93953dd5ad2deb801d131f7daa961e8cf4e
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-21 23:09:24 +0300

    TestGeneratorCodeLensProvider created.

---
+++ src/codelensprovider/TestGeneratorCodeLensProvider.ts
@@ -0,0 +1,22 @@
+import { CodeLensProvider, TextDocument, CodeLens, Command, Range } from "vscode";
+
+
+export class TestGeneratorCodeLensProvider implements CodeLensProvider {
+
+  constructor() {
+
+  }
+
+  async provideCodeLenses(document: TextDocument): Promise<CodeLens[]> {
+    let topOfDocument = new Range(0, 0, 0, 0);
+
+    let c: Command = {
+      command: 'extension.addFunction',
+      title: 'Insert Function',
+    };
+
+    let codeLens = new CodeLens(topOfDocument, c);
+
+    return [codeLens];
+  }
+}
