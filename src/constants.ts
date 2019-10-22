4dc505f30727dffd85b76dada3bd5329d877eb3a
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-22 21:37:29 +0300

    property name fix.

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export class Constants {
   static CLASS_NAME_REGEX = `(class)\s([^\n\s]*)`;
   static keywordList = [
     { regexp: /import/g, name: 'import', anotherKeyword: 'any' },
-    { regexo: Constants.FUNCTION_NAME_REGEX, name: 'function', anotherKeyword: 'any' },
+    { regexp: Constants.FUNCTION_NAME_REGEX, name: 'function', anotherKeyword: 'any' },
     { regexp: /export/g, name: 'export', anotherKeyword: 'any' },
     { regexp: Constants.CLASS_NAME_REGEX, name: 'class', anotherKeyword: 'any' }
   ];
