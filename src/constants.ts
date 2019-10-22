4a779231d4b7ef98374f45399e4455caa44111e1
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-22 21:22:20 +0300

    keyword list created.

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,15 @@
+import { KeywordModel } from "./models/keyword.model";
 
 export class Constants {
   static FUNCTION_NAME_REGEX = /([a-zA-Z_{1}][a-zA-Z0-9_]+)(?=\()/g;
   static CLASS_NAME_REGEX = `(class)\s([^\n\s]*)`;
+  static keywordList = [
+    { regexp: /import/g, name: 'import', anotherKeyword: 'any' },
+    { regexo: Constants.FUNCTION_NAME_REGEX, name: 'function', anotherKeyword: 'any' },
+    { regexp: /export/g, name: 'export', anotherKeyword: 'any' },
+    { regexp: Constants.CLASS_NAME_REGEX, name: 'class', anotherKeyword: 'any' }
+  ];
+
   constructor() {
   }
 }
