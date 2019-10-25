d72b39f0d3e535ea75c1206e7fc50b7d3f26a949
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-25 23:28:39 +0300

    classname regexp changed.

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { KeywordModel } from "./models/keyword.model";
 
 export class Constants {
   static FUNCTION_NAME_REGEX = /([a-zA-Z_{1}][a-zA-Z0-9_]+)(?=\()/g;
-  static CLASS_NAME_REGEX = /(class)\s([^\n\s]*)/g;
+  static CLASS_NAME_REGEX = /(class)\s([^\n\s]*)|(export class)\s([^\n\s]*)|(export default class)\s([^\n\s]*)/g;
   static keywordList = [
     { regexp: /import/g, name: 'import', anotherKeyword: 'any' },
     { regexp: Constants.FUNCTION_NAME_REGEX, name: 'function', anotherKeyword: 'any' },
