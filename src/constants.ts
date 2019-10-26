75ecce7ef01daaf6a744f8eff7995c581aa603ab
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-26 21:47:38 +0300

    added childs for import

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,12 @@ export class Constants {
   static CODE_BLOCK_REGEX = /({[a-z]*\n[\s\S]*?\n})|({[a-z]*[\s\S]*?})/g;
   static CLASS_NAME_REGEX = /(class)\s([^\n\s]*)|(export class)\s([^\n\s]*)|(export default class)\s([^\n\s]*)/g;
   static keywordList = [
-    { regexp: /import/g, name: 'import', anotherKeyword: 'any' },
+    {
+      regexp: /import/g, name: 'import', anotherKeyword: 'any', childs: [
+        { name: 'imports', regexp: Constants.CODE_BLOCK_REGEX, splitBy: ',' },
+        { name: 'from', regexp: Constants.SINGLE_STRING_REGEX }
+      ]
+    },
     { regexp: Constants.FUNCTION_NAME_REGEX, name: 'function', anotherKeyword: 'any' },
     { regexp: /export/g, name: 'export', anotherKeyword: 'any' },
     { regexp: Constants.CLASS_NAME_REGEX, name: 'class', anotherKeyword: 'any' }
