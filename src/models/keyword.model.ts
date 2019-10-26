68abac766ab4244e016bbbc680f19434acef6cb4
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-26 21:46:45 +0300

    KeywordChildModel created.

--- src/models/keyword.model.ts
+++ src/models/keyword.model.ts
@@ -2,4 +2,11 @@ export interface KeywordModel {
   regexp: RegExp;
   name: string;
   anotherKeyword: string;
+  childs: KeywordChildModel[];
+}
+
+export interface KeywordChildModel {
+  regexp: RegExp;
+  name: string;
+  splitBy: string;
 }
