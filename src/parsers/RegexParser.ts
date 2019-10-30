ddd18d39fc4648098f6069f141da0ebc99f1d931
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-10-30 23:11:09 +0300

    matchFilter added in regexParser.

--- src/parsers/RegexParser.ts
+++ src/parsers/RegexParser.ts
@@ -1,10 +1,19 @@
 export class RegexParser {
   getRegexMatch(key: RegExp, text: string): RegExpExecArray {
     try {
-      const regexp = new RegExp(key);
-      return regexp.exec(text) || <RegExpExecArray>{ index: -1 };
+      const regexp = new RegExp(key, 'g');
+      let match = regexp.exec(text) as RegExpExecArray;
+      return this.matchFilter(match);
     } catch(_) {
       return <RegExpExecArray>{ index: -1 };
     }
+  }
+
+  matchFilter(match: RegExpExecArray): RegExpExecArray {
+    if (match !== null) {
+      return match = match.filter((item) => [null, undefined, 'undefined', ''].indexOf(item) < 0) as RegExpExecArray;
+    }
+
+    return <RegExpExecArray>{ index: -1 };
   }
 }
